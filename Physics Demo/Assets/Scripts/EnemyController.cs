2b77db8d5c8efb3efb448db66f9f9726a04ba8ff
Leonard Andrew Spencer <thrawn369@gmail.com>
2018-03-10 17:11:47 +1030

    Added skeleton with ragdoll

---
+++ Physics Demo/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    RagdollManager ragdoll;
+
+	// Use this for initialization
+	void Start () {
+        ragdoll = GetComponent<RagdollManager>();
+        ragdoll.setRagdollActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Physics Demo/Assets/Scripts/RagdollManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollManager : MonoBehaviour {
+
+    GameObject Root;
+    List<Rigidbody> RagdollBodies;
+
+    private void Awake()
+    {
+        RagdollBodies = new List<Rigidbody>(Root.GetComponentsInChildren<Rigidbody>());
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setRagdollActive(bool value)
+    {
+        foreach(Rigidbody body in RagdollBodies)
+        {
+            body.isKinematic = !value;
+            // HACK maybe also turn colliders on and off?
+        }
+    }
+}
