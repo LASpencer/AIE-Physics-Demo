fdbce8a7a6e2d79507d203ae9c63baf6717b5197
Andrew Spencer <thrawn369@gmail.com>
2018-03-14 11:49:11 +1030

    Put in slideable wall

---
+++ Physics Demo/Assets/Scripts/SlidingWall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO change target position x between 0 and -8
+	}
+}
